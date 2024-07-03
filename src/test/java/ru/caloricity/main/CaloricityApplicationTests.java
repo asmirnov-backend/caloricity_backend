50846d0fbbab86d4ebc50d7ae6a7f73ae8e2a6fe
Smirnov Andrew <smirnov.andrew.1999@yandex.ru>
2024-07-03 14:52:51 +0300

    Init spting

---
+++ src/test/java/ru/caloricity/main/CaloricityApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.caloricity.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaloricityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
