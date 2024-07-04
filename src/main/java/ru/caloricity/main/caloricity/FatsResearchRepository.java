c7ffd2411c361e820bd808927ff28f0982b418d5
Andrew Smirnov <smirnov.andrew.1999@yandex.ru>
2024-07-04 19:06:38 +0300

    Add repositories

---
+++ src/main/java/ru/caloricity/main/caloricity/FatsResearchRepository.java
@@ -0,0 +1,10 @@
+package ru.caloricity.main.caloricity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.UUID;
+
+@RepositoryRestResource(exported = false)
+public interface FatsResearchRepository extends JpaRepository<FatsResearch, UUID> {
+}
