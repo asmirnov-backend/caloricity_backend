1af1b99f3b0eb24077a058eedb2e8b0f732af04e
Andrew Smirnov <smirnov.andrew.1999@yandex.ru>
2024-09-06 22:36:15 +0300

    Remove redundant test

--- src/test/java/ru/caloricity/CaloricityApplicationTests.java
+++ src/test/java/ru/caloricity/CaloricityApplicationTests.java
@@ -21,42 +21,4 @@ class CaloricityApplicationTests {
                 .writeModulesAsPlantUml()
                 .writeIndividualModulesAsPlantUml();
     }
-
-    @Test
-    void test() {
-        System.out.print(characterReplacement("AABABBA", 1));
-    }
-
-    public int characterReplacement(String s, int k) {
-        char[] chars = s.toCharArray();
-        int left = 0;
-        int right = 1;
-        int longest = 1;
-        int localK = k;
-
-        while (left < chars.length - 1) {
-            if (right == chars.length) {
-                int leftK = localK > 0 ? (localK < left + 1 ? localK : left + 1 - 1) : 0;
-                var ii = Math.max(right - left + leftK, longest);
-                return ii;
-            }
-
-            if (chars[left] == chars[right]) {
-                right++;
-            } else {
-                if (localK > 0) {
-                    localK--;
-                    right++;
-                } else {
-                    longest = Math.max(right - left, longest);
-                    left++;
-                    right = left + 1;
-                    localK = k;
-                }
-            }
-        }
-
-        return longest;
-    }
-
 }
