d7696dce7d8c1be6666362b776bba04f8327075b
Andrew Smirnov <smirnov.andrew.1999@yandex.ru>
2024-10-27 15:18:54 +0300

    feat: add ExceptionHandler for OptimisticLockException

--- src/main/java/ru/caloricity/common/ExceptionApiHandler.java
+++ src/main/java/ru/caloricity/common/ExceptionApiHandler.java
@@ -1,5 +1,6 @@
 package ru.caloricity.common;
 
+import jakarta.persistence.OptimisticLockException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.ErrorResponse;
@@ -19,6 +20,12 @@ public class ExceptionApiHandler {
     @ExceptionHandler(CascadeDeleteRestrictException.class)
     public ErrorResponse notFoundException(CascadeDeleteRestrictException ex) {
         return ErrorResponse.builder(ex, ProblemDetail.forStatus(HttpStatus.BAD_REQUEST)).title("Связанные сущности не позволяют произвести удаление").detail("У " + ex.getEntityClass().getSimpleName() + " с id=" + ex.getEntityId() + " есть зависимости с " + ex.getLinkedEntityClass().getSimpleName()).build();
+    }
+
+    @ExceptionHandler(OptimisticLockException.class)
+    public ErrorResponse optimisticLockException(OptimisticLockException ex) {
+        return ErrorResponse.builder(ex, ProblemDetail.forStatus(HttpStatus.CONFLICT)).title("Данные, с которыми вы работали, были изменены другим пользователем. Пожалуйста, начните\n" +
+                "выполнение задачи повторно с измененными данными.").detail(ex.getMessage()).build();
     }
 
 }
